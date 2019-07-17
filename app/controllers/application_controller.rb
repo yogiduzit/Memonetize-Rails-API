47e80391d828e0887b84dd8dea16c3335d4f71c1
yogiduzit <yverma2@my.bcit.ca>
2019-07-16 18:23:30 -0700

    Created user helper functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  private
+  def current_user
+    @current_user ||= User.find_by_id session[:user_id]
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+
+  def authenticate_user!
+    unless user_signed_in?
+      flash[:danger] = "Please sign in"
+      redirect_to new_sessions_path
+    end
+  end
 end
