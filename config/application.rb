1b0e9fde0a89c42aca0045e48c03b67bb9be73c1
yogiduzit <yverma2@my.bcit.ca>
2019-07-29 17:49:22 -0700

    Require gems and some useless files

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,14 @@ require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
+require "json"
+require "ibm_watson/visual_recognition_v3"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module GotGovt
+module Sell_Yo_Meme
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
