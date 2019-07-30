aa58f52161784cedacf43c7c96d776efa6c0af03
yogiduzit <yverma2@my.bcit.ca>
2019-07-29 17:42:20 -0700

    Removed confirm password method (bcrypt already performs it)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,20 @@
 class User < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  
-  validates :password, presence: true
-  validate :confirm_password
 
-  def confirm_password
-    if password != password_confirmation
-      errors.add(:password_confirmation, "must be equal to password")
-    end
-  end
+  has_many :memes, dependent: :destroy
+
+  # def confirm_password
+  #   if password != password_confirmation
+  #     errors.add(:password_confirmation, "must be equal to password")
+  #   end
+  # end
 
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   validates :email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  
+  validates :password, presence: true
 
   has_secure_password
 end
