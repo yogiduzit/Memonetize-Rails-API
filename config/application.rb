8ed4a7692805963950143cd82585c9617cb2ca18
yogiduzit <yverma2@my.bcit.ca>
2019-08-08 17:32:47 -0700

    Set up CORS

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,24 @@ module Sell_Yo_Meme
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.middleware.insert_before(0, Rack::Cors) do
+      allow do
+        # origins specifies which domains are allowed to make AJAX requests
+        # to this server
+        origins("localhost:8080", "127.0.0.1:8080", "127.0.0.1:5500")
+
+         resource(
+          # allow access to only paths that begin with /api/
+          "/api/*",
+          # this allows all HTTP headers to be sent
+          headers: :any,
+          # Allows sharing of cookies for CORS requests made to this resource
+          credentials: true,
+          # define the HTTP verbs which are allowed in a request
+          methods: [:get, :post, :delete, :patch, :put, :options]
+        )
+      end
+    end
   end
 end
