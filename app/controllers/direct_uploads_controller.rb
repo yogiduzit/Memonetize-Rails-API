c82b9eb87d09bfbc6bd40f5b02995f37caf3a425
yogiduzit <yverma2@my.bcit.ca>
2019-08-20 18:43:59 -0700

    fluke

--- app/controllers/direct_uploads_controller.rb
+++ app/controllers/direct_uploads_controller.rb
@@ -1,4 +1,5 @@
 class DirectUploadsController < ActiveStorage::DirectUploadsController
+  p "Yogi"
   protect_from_forgery with: :null_session  
   skip_before_action :verify_authenticity_token
 end
