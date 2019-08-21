69cddce21b0906a59c66e3f9cb57efa89cb297d4
yogiduzit <yverma2@my.bcit.ca>
2019-08-20 18:36:59 -0700

    Change CSRF settings

--- app/controllers/direct_uploads_controller.rb
+++ app/controllers/direct_uploads_controller.rb
@@ -1,4 +1,4 @@
 class DirectUploadsController < ActiveStorage::DirectUploadsController
-  protect_from_forgery with: :exception  
+  protect_from_forgery with: :null_session  
   skip_before_action :verify_authenticity_token
 end
