953d20caca51298a7aa41269c1439f604c53d801
yogiduzit <yverma2@my.bcit.ca>
2019-08-22 22:34:27 -0700

    Method to check if user is authorized to CRUD

--- app/serializers/meme_serializer.rb
+++ app/serializers/meme_serializer.rb
@@ -4,11 +4,16 @@ class MemeSerializer < ActiveModel::Serializer
     :title,
     :body,
     :created_at,
-    :meme_img
+    :meme_img,
+    :authorized
   )
   belongs_to(:user, key: :author)
 
   def meme_img
     S3_BUCKET.object(object.meme_img.key).public_url
+  end
+
+  def authorized
+    object.user == current_user
   end
 end
