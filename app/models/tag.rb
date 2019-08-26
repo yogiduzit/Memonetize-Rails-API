85f57b6ab144f26d582f2f8e7f7fea9c1cb03688
yogiduzit <yverma2@my.bcit.ca>
2019-08-25 19:08:53 -0700

    Set up association and fixed auth bug

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,12 @@
 class Tag < ApplicationRecord
+
+  has_many :meme_taggings
+  has_many :user_taggings
+  
   has_many :memes, through: :meme_taggings
   has_many :users, through: :user_taggings
+
+
 
   before_validation :downcase_name
 
