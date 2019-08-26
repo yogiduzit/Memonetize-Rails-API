e6ecb07b18f532eb8fa3deb3b19375e460700954
yogiduzit <yverma2@my.bcit.ca>
2019-08-26 14:43:46 -0700

    Set up votes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_24_004224) do
+ActiveRecord::Schema.define(version: 2019_08_26_205156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,10 +81,22 @@ ActiveRecord::Schema.define(version: 2019_08_24_004224) do
     t.boolean "is_pro", default: false
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.string "vote_type"
+    t.bigint "user_id"
+    t.bigint "meme_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["meme_id"], name: "index_votes_on_meme_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "meme_taggings", "memes"
   add_foreign_key "meme_taggings", "tags"
   add_foreign_key "memes", "users"
   add_foreign_key "user_taggings", "tags"
   add_foreign_key "user_taggings", "users"
+  add_foreign_key "votes", "memes"
+  add_foreign_key "votes", "users"
 end
